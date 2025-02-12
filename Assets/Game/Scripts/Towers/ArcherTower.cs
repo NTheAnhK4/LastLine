1379697ed5b7759f88f48c258f1a19728333f6ac
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2025-02-12 22:18:18 +0700

    Archer tower detect enemy

--- Assets/Game/Scripts/Towers/ArcherTower.cs
+++ Assets/Game/Scripts/Towers/ArcherTower.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 public class ArcherTower : Tower
 {
     [SerializeField] private RangedAttack archerAttack;
+    [SerializeField] private GameObject projectilePrefab;
     protected override void LoadComponent()
     {
         base.LoadComponent();
         if (archerAttack == null) archerAttack = transform.GetComponentInChildren<RangedAttack>();
         
-        archerAttack.Init(transform,3);
+        archerAttack.Init(transform,3,0.4f,projectilePrefab);
     }
 }
