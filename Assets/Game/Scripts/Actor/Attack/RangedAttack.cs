fb7be7b57d3d74818b552cfb65cc785902b7c44a
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2025-02-13 10:31:20 +0700

    Archer tower shoot enemy

--- Assets/Game/Scripts/Actor/Attack/RangedAttack.cs
+++ Assets/Game/Scripts/Actor/Attack/RangedAttack.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class RangedAttack : AttackHandler
@@ -14,6 +13,14 @@ public class RangedAttack : AttackHandler
     protected override void Attack(HealthHandler enemy)
     {
         base.Attack(enemy);
-        PoolingManager.Spawn(projectilePrefab, actor.position, default, transform);
+        
+        Vector3 enemyPosition = enemy.Actor.position;
+        var position = actor.position;
+        Vector3 direction = (enemyPosition - position);
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Projectile projectile = PoolingManager.Spawn(projectilePrefab, position, Quaternion.Euler(new Vector3(0,0,angle)), transform)
+            .GetComponent<Projectile>();
+        if(projectile != null) projectile.Init(enemy.Actor);
     }
 }
