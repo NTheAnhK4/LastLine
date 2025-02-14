4e5d40d62949210082f6b5cf2178fe3dc44dd4bf
Nguyen The Anh <nguyentheanh14102004@gmail.com>
2025-02-14 10:05:21 +0700

    Add tower anim

--- Assets/Game/Scripts/Actor/Attack/RangedAttack.cs
+++ Assets/Game/Scripts/Actor/Attack/RangedAttack.cs
@@ -19,6 +19,11 @@ public class RangedAttack : AttackHandler
         Vector3 direction = (enemyPosition - position);
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        if(angle >= 45 && angle < 135) animHandler.SetInt("direction",0);
+        else if((angle >= 0 && angle < 45) || (angle < 0 && angle >= -45)) animHandler.SetInt("direction",1);
+        else if(angle >= -135 && angle < -45) animHandler.SetInt("direction", 2);
+        else animHandler.SetInt("direction", 3);
+        animHandler.SetAnim("Attack");
         Projectile projectile = PoolingManager.Spawn(projectilePrefab, position, Quaternion.Euler(new Vector3(0,0,angle)), transform)
             .GetComponent<Projectile>();
         if(projectile != null) projectile.Init(enemy.Actor);
